87a528cfafd0f7e61f7e010edd98e53d68cd09d2
Pradeep Verma <pv4042609@gmail.com>
2023-10-20 15:27:24 +0530

    Add files via upload

---
+++ Assignment01/arithmetic_operation.java
@@ -0,0 +1,17 @@
+class Arithmetic{
+      public static void main(String [] args){
+	       int a=20;
+		   int b =22;
+		   
+		   // addition 
+		   System.out.println(a+b);
+		   //multiplication
+		   System.out.println(a*b);
+		   //subtraction
+		   System.out.println(a-b);
+		   //remainder
+		   System.out.println(a%b);
+		   
+		   }
+		   }
+		   
---
+++ Assignment01/compound_interest.java
@@ -0,0 +1,15 @@
+class CompoundInterest{
+      public static void main(String [] args)
+	  {
+	       float p=15000f;
+		   float r=2.5f;
+		   float t = 2f;
+		   
+		   float ans = (p*r*t)/100;
+		   
+		   System.out.println(ans);
+		   
+		   }
+		   }
+		   
+		   
